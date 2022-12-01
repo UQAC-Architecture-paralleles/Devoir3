6bddd6dded48db5248d7b35c75611a8f2f903122
louislombard <louis.raphael.lombard@gmail.com>
2022-11-30 22:03:14 -0500

    test

--- test.c
+++ test.c
@@ -120,6 +120,7 @@ int main(int argc, char *argv[])
     prime = 2;
     do
     {
+        printf("Debug from id %d:---------------------------------", id);
         printf("Debug from id %d: prime=%d\n", id, prime);
         printf("Debug from id %d: index=%d\n", id, index);
         if (prime * prime > low_value)
@@ -156,8 +157,9 @@ int main(int argc, char *argv[])
             }
             printf("Debug from id %d: prime=%d\n", id, prime);
         }
-
+        printf("Debug from id %d:---------------------------------", id);
         MPI_Bcast(&prime, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
     } while (prime * prime <= n);
 
     count = 0;
