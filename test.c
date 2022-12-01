c1c99fbf16aac1b2f124fb95976f331f68a1b7da
louislombard <louis.raphael.lombard@gmail.com>
2022-11-30 20:22:02 -0500

    test

--- test.c
+++ test.c
@@ -1,44 +1,86 @@
 #include <mpi.h>
+#include <math.h>
 #include <stdio.h>
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
+int main(int argc, char *argv[])
+{
 
-#define EXTRACT_BIT(n,i) ((n&(1<<i))?1:0)
-void check_circuit (int id, int z) {
-   int v[16];        /* Each element is a bit of z */
-   int i;
-   for (i = 0; i < 16; i++) v[i] = EXTRACT_BIT(z,i);
-   if ((v[0] || v[1]) && (!v[1] || !v[3]) && (v[2] || v[3])
-      && (!v[3] || !v[4]) && (v[4] || !v[5])
-      && (v[5] || !v[6]) && (v[5] || v[6])
-      && (v[6] || !v[15]) && (v[7] || !v[8])
-      && (!v[7] || !v[13]) && (v[8] || v[9])
-      && (v[8] || !v[9]) && (!v[9] || !v[10])
-      && (v[9] || v[11]) && (v[10] || v[11])
-      && (v[12] || v[13]) && (v[13] || !v[14])
-      && (v[14] || v[15])) {
-        printf ("%d) %d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n", id, v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7],v[8],v[9], v[10],v[11],v[12],v[13],v[14],v[15]);
-        fflush (stdout);
-   }
-}
-
-int main (int argc, char *argv[]) {
-    int i;
-    int id;
-    int p;
-    void check_circuit (int, int);
-
-    MPI_Init (&argc, &argv); 
-    MPI_Comm_rank (MPI_COMM_WORLD, &id);
-    MPI_Comm_size (MPI_COMM_WORLD, &p);
-
-    for (i = id; i < 65536; i += p){
-        check_circuit (id, i);
+    MPI_Init(&argc, &argv);
+    MPI_Barrier(MPI_COMM_WORLD);
+    elapsed_time = -MPI_Wtime();
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    if (argc != 2)
+    {
+        if (!id)
+        {
+            printf("Command line: %s <m>\n", argv[0]);
+        }
+        MPI_Finalize();
+        exit(1);
     }
-        
-    printf ("Process %d is done\n", id);
-    fflush (stdout);
+
+    n = atoi(argv[1]);
+    low_value = 2 + BLOCK_LOW(id, p, n - 1);
+    high_value = 2 + BLOCK_HIGH(id, p, n - 1);
+    size = BLOCK_SIZE(id, p, n - 1);
+    proc0_size = (n - 1) / p;
+    if ((2 + proc0_size) < (int)sqrt((double)n))
+    {
+        if (!id)
+            printf("Too many processes\n");
+        MPI_Finalize();
+        exit(1);
+    }
+    marked = (char *)malloc(size);
+    if (marked == NULL)
+    {
+        printf("Cannot allocate enough memory\n");
+        MPI_Finalize();
+        exit(1);
+    }
+
+    for (i = 0; i < size; i++)
+        marked[i] = 0;
+    if (!id)
+        index = 0;
+    prime = 2;
+    do
+    {
+        if (prime * prime > low_value)
+            first = prime * prime - low_value;
+        else
+        {
+            if (!(low_value % prime))
+                first = 0;
+            else
+                first = prime - (low_value % prime);
+        }
+        for (i = first; i < size; i += prime)
+            marked[i] = 1;
+        if (!id)
+        {
+            while (marked[++index])
+                ;
+            prime = index + 2;
+        }
+        MPI_Bcast(&prime, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    } while (prime * prime <= n);
+
+    count = 0;
+    for (i = 0; i < size; i++)
+        if (!marked[i])
+            count++;
+    MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM,
+               0, MPI_COMM_WORLD);
+    elapsed_time += MPI_Wtime();
+    if (!id)
+    {
+        printf("%d primes are less than or equal to %d\n", global_count, n);
+        printf("Total elapsed time: %10.6f\n", elapsed_time);
+    }
     MPI_Finalize();
     return 0;
 }
-
