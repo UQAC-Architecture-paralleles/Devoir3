25f3e8d8249017166dbf4000d10fd22debc9cf13
louislombard <louis.raphael.lombard@gmail.com>
2022-11-30 20:51:53 -0500

    test

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-#define BLOCK_LOW(id, p, n) ((id) * (n)) / (p))
+#define BLOCK_LOW(id, p, n) (((id) * (n)) / (p))
 #define BLOCK_HIGH(id, p, n) (BLOCK_LOW((id) + 1, p, n) - 1)
 #define BLOCK_SIZE(id, p, n) (BLOCK_LOW((id) + 1, p, n) - BLOCK_LOW(id, p, n))
 #define BLOCK_OWNER(index, p, n) ((((p) * (index) + 1) - 1) / (n))
