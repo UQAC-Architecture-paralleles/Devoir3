b2f0e6ee36e83761245cf379a2a572114cd10357
louislombard <louis.raphael.lombard@gmail.com>
2022-11-30 23:58:48 -0500

    test

--- test.c
+++ test.c
@@ -150,10 +150,10 @@ int main(int argc, char *argv[])
         if (id == BLOCK_OWNER(index, p, n))
         {
             printf("Debug - LOOOOL1 -  from id %d: index=%d\n", id, index);
-            int local_index = index - (low_value - 2);
-            while (!marked[++local_index])
+            // int local_index = index - (low_value - 2);
+            while (!marked[++index])
             {
-                printf("Debug - LOOOOL2 -  from id %d: local_index=%d\n", id, local_index);
+                printf("Debug - LOOOOL2 -  from id %d: local_index=%d\n", id, index);
                 prime = index + 2;
             }
             printf("Debug - LOOOOL3 -  from id %d: prime=%d\n", id, prime);
