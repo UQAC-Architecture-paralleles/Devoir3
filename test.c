6d20e6c4f7554086932ef75572546ebc05f0f607
louislombard <louis.raphael.lombard@gmail.com>
2022-11-30 20:29:58 -0500

    test

--- test.c
+++ test.c
@@ -6,12 +6,9 @@
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 #define BLOCK_LOW(id, p, n) ((i) * (n) / (p))
-#define BLOCK_HIGH(id, p, n) \
-    (BLOCK_LOW((id) + 1, p, n) - 1)
-#define BLOCK_SIZE(id, p, n) \
-    (BLOCK_LOW((id) + 1) - BLOCK_LOW(id))
-#define BLOCK_OWNER(index, p, n) \
-        (((p)*(index)+1)-1)/(n))
+#define BLOCK_HIGH(id, p, n) (BLOCK_LOW((id) + 1, p, n) - 1)
+#define BLOCK_SIZE(id, p, n) (BLOCK_LOW((id) + 1) - BLOCK_LOW(id))
+#define BLOCK_OWNER(index, p, n) ((((p) * (index) + 1) - 1) / (n))
 
 int main(int argc, char *argv[])
 {
