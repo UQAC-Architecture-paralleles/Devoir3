14ef9f5ad43829cb7f2e2379e418de48f90f47f1
louislombard <louis.raphael.lombard@gmail.com>
2022-11-30 20:51:30 -0500

    test

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-#define BLOCK_LOW(id, p, n) ((i) * (n) / (p))
+#define BLOCK_LOW(id, p, n) ((id) * (n)) / (p))
 #define BLOCK_HIGH(id, p, n) (BLOCK_LOW((id) + 1, p, n) - 1)
 #define BLOCK_SIZE(id, p, n) (BLOCK_LOW((id) + 1, p, n) - BLOCK_LOW(id, p, n))
 #define BLOCK_OWNER(index, p, n) ((((p) * (index) + 1) - 1) / (n))
@@ -48,7 +48,6 @@ int main(int argc, char *argv[])
     }
 
     n = atoi(argv[1]);
-    printf("Debug: %d , from id %d\n", n, id);
     low_value = 2 + BLOCK_LOW(id, p, n - 1);
     high_value = 2 + BLOCK_HIGH(id, p, n - 1);
     size = BLOCK_SIZE(id, p, n - 1);
